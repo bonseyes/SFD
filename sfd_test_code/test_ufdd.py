7fdbfc06d324a75a93ecebc7a55171e7f0c56129
Deepak Ghimire <ghmdeepak@gmail.com>
2018-06-18 19:54:01 -0700

    ufdd test python script

---
+++ sfd_test_code/test_ufdd.py
@@ -0,0 +1,166 @@
+from os import makedirs
+from os.path import abspath, dirname, exists, join, basename
+from sfd_detector import SFD_NET
+import argparse
+import caffe
+import cv2
+import numpy as np
+import scipy.io as sio
+
+# Default models if none is passed to the __init__ method, assuming the `sfd_test_code` is in ${CAFFE_ROOT}/SFD/sfd_test_code
+# These models are automatically downloaded by SFD/scripts/data/download_model.sh
+MODEL_DEF = '{}/../../models/VGGNet/WIDER_FACE/SFD_trained/deploy.prototxt'.format(abspath(dirname(__file__)))
+MODEL_WEIGHTS = '{}/../../models/VGGNet/WIDER_FACE/SFD_trained/SFD.caffemodel'.format(abspath(dirname(__file__)))
+
+
+def multi_scale_test(net, image, max_im_shrink):
+    # Shrink detecting and shrink only detect big face
+    st = 0.5 if max_im_shrink >= 0.75 else 0.5 * max_im_shrink
+    det_s = net.detect(image, st)
+    index = np.where(np.maximum(det_s[:, 2] - det_s[:, 0] + 1, det_s[:, 3] - det_s[:, 1] + 1) > 30)[0]
+    det_s = det_s[index, :]
+
+    # Enlarge one times
+    bt = min(2, max_im_shrink) if max_im_shrink > 1 else (st + max_im_shrink) / 2
+    det_b = net.detect(image, bt)
+
+    # Enlarge small image x times for small face
+    if max_im_shrink > 2:
+        bt *= 2
+        while bt < max_im_shrink:
+            det_ = net.detect(image, bt)
+            det_b = np.row_stack((det_b, det_))
+            bt *= 2
+        det_ = net.detect(image, bt)
+        det_b = np.row_stack((det_b, det_))
+
+    # Enlarge only detect small face
+    if bt > 1:
+        index = np.where(np.minimum(det_b[:, 2] - det_b[:, 0] + 1, det_b[:, 3] - det_b[:, 1] + 1) < 100)[0]
+        det_b = det_b[index, :]
+    else:
+        index = np.where(np.maximum(det_b[:, 2] - det_b[:, 0] + 1, det_b[:, 3] - det_b[:, 1] + 1) > 30)[0]
+        det_b = det_b[index, :]
+
+    return det_s, det_b
+
+
+def flip_test(net, image, shrink):
+    orig_shape = image.shape
+    image_f = cv2.flip(image, 1)
+    image_f = image_f.reshape(orig_shape)
+    det_f = net.detect(image_f, shrink)
+
+    det_t = np.zeros(det_f.shape)
+    det_t[:, 0] = image.shape[1] - det_f[:, 2]
+    det_t[:, 1] = det_f[:, 1]
+    det_t[:, 2] = image.shape[1] - det_f[:, 0]
+    det_t[:, 3] = det_f[:, 3]
+    det_t[:, 4] = det_f[:, 4]
+    return det_t
+
+
+def bbox_vote(det):
+    order = det[:, 4].ravel().argsort()[::-1]
+    det = det[order, :]
+    dets = np.zeros((0, 5))
+    while det.shape[0] > 0:
+        # IOU
+        area = (det[:, 2] - det[:, 0] + 1) * (det[:, 3] - det[:, 1] + 1)
+        xx1 = np.maximum(det[0, 0], det[:, 0])
+        yy1 = np.maximum(det[0, 1], det[:, 1])
+        xx2 = np.minimum(det[0, 2], det[:, 2])
+        yy2 = np.minimum(det[0, 3], det[:, 3])
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        o = inter / (area[0] + area[:] - inter)
+
+        # Get needed merge det and delete these det
+        merge_index = np.where(o >= 0.3)[0]
+        det_accu = det[merge_index, :]
+        det = np.delete(det, merge_index, 0)
+
+        if merge_index.shape[0] <= 1:
+            continue
+        det_accu[:, 0:4] = det_accu[:, 0:4] * np.tile(det_accu[:, -1:], (1, 4))
+        max_score = np.max(det_accu[:, 4])
+        det_accu_sum = np.zeros((1, 5))
+        det_accu_sum[:, 0:4] = np.sum(det_accu[:, 0:4], axis=0) / np.sum(det_accu[:, -1:])
+        det_accu_sum[:, 4] = max_score
+        try:
+            dets = np.row_stack((dets, det_accu_sum))
+        except:
+            dets = det_accu_sum
+
+    dets = dets[0:750, :]
+    return dets
+
+
+def write_to_txt(f, det):
+    f.write('{:s}\n'.format(event[0][0].encode('utf-8') + '/' + im_name + '.jpg'))
+    f.write('{:d}\n'.format(det.shape[0]))
+    for i in xrange(det.shape[0]):
+        xmin = det[i][0]
+        ymin = det[i][1]
+        xmax = det[i][2]
+        ymax = det[i][3]
+        score = det[i][4]
+        f.write('{:.1f} {:.1f} {:.1f} {:.1f} {:.3f}\n'.
+                format(xmin, ymin, (xmax - xmin + 1), (ymax - ymin + 1), score))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Test code for a SFD trained model using UFDD dataset.')
+    parser.add_argument('-p', '--path', type=str, help='Dataset path', required=True)
+    parser.add_argument('-g', '--greyscale', action='store_true', help='When set, assume that all the images are greyscale')
+    parser.add_argument('-o', '--output', type=str, help='Folder where to output detections', required=True)
+    parser.add_argument('--model', type=str, help='Path to Caffe prototxt', default=MODEL_DEF, required=False)
+    parser.add_argument('--weights', type=str, help='Path to Caffe weights (caffemodel)', default=MODEL_WEIGHTS, required=False)
+    parser.add_argument('--device', type=int, default=0, help="GPU device to use, default is 0")
+    args = parser.parse_args()
+
+    dataset_path = args.path
+    model = args.model
+    weights = args.weights
+    model_name = basename(weights).replace('.caffemodel', '')
+    device = args.device
+    is_greyscale = args.greyscale
+    output_folder = join(args.output, model_name)
+
+    net = SFD_NET(model_file=model, pretrained_file=weights, device=device)
+
+    ufdd_face = sio.loadmat('./output/UFDD/UFDD_val_bbx_gt.mat')
+    event_list = ufdd_face['event_list']
+    file_list = ufdd_face['file_list']
+    del ufdd_face
+
+    for index, event in enumerate(event_list):
+        filelist = file_list[index][0]
+        event_folder = join(output_folder, event[0][0])
+        if not exists(event_folder):
+            makedirs(event_folder)
+
+        for num, file in enumerate(filelist):
+            im_name = file[0][0]
+            Image_Path = dataset_path + event[0][0] + '/' + im_name[:] + '.jpg'
+            image = caffe.io.load_image(Image_Path)
+            if is_greyscale:
+                image = image[:, :, 0]
+                image = image.reshape((image.shape[0], image.shape[1], 1))
+
+            # The max size of input blob for caffe
+            max_im_shrink = (0x7fffffff / 800.0 / (image.shape[0] * image.shape[1] * 3)) ** 0.5
+            shrink = max_im_shrink if max_im_shrink < 1 else 1
+
+            det0 = net.detect(image, shrink)
+            det1 = flip_test(net, image, shrink)    # flip test
+            [det2, det3] = multi_scale_test(net, image, max_im_shrink)  #multi-scale test
+
+            # Merge all test results via bounding box voting
+            det = np.row_stack((det0, det1, det2, det3))
+            dets = bbox_vote(det)
+
+            f = open(join(event_folder, im_name + '.txt'), 'w')
+            write_to_txt(f, dets)
+            print('event:%d num:%d' % (index + 1, num + 1))
