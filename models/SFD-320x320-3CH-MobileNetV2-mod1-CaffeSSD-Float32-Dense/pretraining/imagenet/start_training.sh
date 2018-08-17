6883d63bc41c27507f5c258a4bd4f4101a9ccb0e
ezetl <ezetl91@gmail.com>
2018-08-17 19:09:40 -0300

    imagenet pretrain files for MNV2 mod1

--- models/SFD-320x320-3CH-MobileNetV2-mod1-CaffeSSD-Float32-Dense/pretraining/imagenet/start_training.sh
+++ models/SFD-320x320-3CH-MobileNetV2-mod1-CaffeSSD-Float32-Dense/pretraining/imagenet/start_training.sh
@@ -4,4 +4,4 @@ mkdir -p logs snapshots
 
 ${CAFFE_ROOT}/build/tools/caffe train \
 --solver=solver.prototxt \
---gpu=0,1,2,3 2>&1 | tee logs/MobileNetV2-mod1-ILSVRC12-Dense.log
+--gpu=0 2>&1 | tee logs/MobileNetV2-mod1-ILSVRC12-Dense.log
