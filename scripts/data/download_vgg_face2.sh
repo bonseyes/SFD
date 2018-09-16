35aa05c5b91d75638c7aec2f575a5890fb326f21
Deepak Ghimire <ghmdeepak@gmail.com>
2018-09-16 22:21:00 +0900

    update download links

--- scripts/data/download_vgg_face2.sh
+++ scripts/data/download_vgg_face2.sh
@@ -6,28 +6,22 @@ source $root_dir/scripts/data/datasets_utils.sh
 dst_dir=$1
 output_dir=$(create_folder "$dst_dir" "VGG_FACE2")
 
-wget http://www.robots.ox.ac.uk/~vgg/data/vgg_face2/licence.txt -O $output_dir/licence.txt
-wget http://www.robots.ox.ac.uk/~vgg/data/vgg_face2/Readme.txt -O $output_dir/Readme.txt
+#id=nviso
+#pass=nvisovgg
 
-wget --user=nviso --password=nvisovgg zeus.robots.ox.ac.uk/vgg_face2/get_file?fname=vggface2_train.tar.gz -O $output_dir/vggface2_train.tar.gz
-wget --user=nviso --password=nvisovgg zeus.robots.ox.ac.uk/vgg_face2/get_file?fname=vggface2_test.tar.gz -O $output_dir/vggface2_test.tar.gz
+wget --user=nviso --password=nvisovgg 'zeus.robots.ox.ac.uk/vgg_face2/get_file?fname=vggface2_train.tar.gz' -O $output_dir/vggface2_train.tar.gz
+wget --user=nviso --password=nvisovgg 'zeus.robots.ox.ac.uk/vgg_face2/get_file?fname=vggface2_test.tar.gz' -O $output_dir/vggface2_test.tar.gz
 
-wget http://www.robots.ox.ac.uk/~vgg/data/vgg_face2/MD5 -O $output_dir/MD5
-
-wget http://www.robots.ox.ac.uk/~vgg/data/vgg_face2/meta.tar.gz -O $output_dir/meta.tar.gz
-
-wget http://www.robots.ox.ac.uk/~vgg/data/vgg_face2/bb_landmark.tar.gz -O $output_dir/bb_landmark.tar.gz
-wget http://www.robots.ox.ac.uk/~vgg/data/vgg_face2/vggface2_caffe_model.tar.gz -O $output_dir/vggface2_caffe_model.tar.gz
-wget http://www.robots.ox.ac.uk/~vgg/data/vgg_face2/vggface2_matconvnet_model.tar.gz -O $output_dir/vggface2_matconvnet_model.tar.gz
-wget http://www.robots.ox.ac.uk/~vgg/data/vgg_face2/dev_kit.tar.gz -O $output_dir/dev_kit.tar.gz
-
+wget http://www.robots.ox.ac.uk/~vgg/data/vgg_face2/meta/train_list.txt -O $output_dir/train_list.txt
+wget http://www.robots.ox.ac.uk/~vgg/data/vgg_face2/meta/test_list.txt -O $output_dir/test_list.txt
+wget www.robots.ox.ac.uk/~vgg/data/vgg_face2/meta/identity_meta.csv -O $output_dir/identity_meta.csv
+wget www.robots.ox.ac.uk/~vgg/data/vgg_face2/meta/class_overlap_vgg1_2.txt -O $output_dir/class_overlap_vgg1_2.txt
+wget http://www.robots.ox.ac.uk/~vgg/data/vgg_face2/meta/test_posetemp_imglist.txt -O $output_dir/test_posetemp_imglist.txt
+wget http://www.robots.ox.ac.uk/~vgg/data/vgg_face2/meta/test_agetemp_imglist.txt -O $output_dir/test_agetemp_imglist.txt
+wget www.robots.ox.ac.uk/~vgg/data/vgg_face2/meta/bb_landmark.tar.gz -O $output_dir/bb_landmark.tar.gz
 
 cd $output_dir
 
 tar -xzf vggface2_train.tar.gz
 tar -xzf vggface2_test.tar.gz
-tar -xvf meta.tar.gz
 tar -xvf bb_landmark.tar.gz
-tar -xvf vggface2_caffe_model.tar.gz
-tar -xvf vggface2_matconvnet_model.tar.gz
-tar -xvf dev_kit.tar.gz
