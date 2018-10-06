14a2e71f0760fed5528162c715b2655663c231f0
Deepak Ghimire <ghmdeepak@gmail.com>
2018-10-06 16:23:30 +0900

    download script for CrowdHuman dataset is added.

---
+++ scripts/data/download_CrowdHuman.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Data set homepage: http://www.crowdhuman.org/
+
+root_dir=$(git rev-parse --show-toplevel)
+source $root_dir/scripts/data/datasets_utils.sh 
+
+dst_dir=$1
+output_dir=$(create_folder "$dst_dir" "CrowdHuman")
+drive=$root_dir/scripts/data/google-drive.py
+
+python $drive 1XZm5L27eQasrvMq008l6vW8I8ysnavSM $output_dir/CrowdHuman_train01.zip
+python $drive 1KgreKkhPfIiZHkl-x5K7p5LHKsfZss5q $output_dir/CrowdHuman_train02.zip
+python $drive 1Af1rBvQSxOmXphoNNtKzAJyrEeeAF-aV $output_dir/CrowdHuman_train03.zip
+python $drive 1dRRL6eKE1v_1Kb_R8nZGhTQ0HzYZ-Pss $output_dir/CrowdHuman_val.zip
+python $drive 1uBB3psTLteVEP2Wg466DsYVx1eV8LacU $output_dir/annotation_train.odgt
+python $drive 151-MHsdCni1izANEuZA3q3Pp7Jwpi-PX $output_dir/annotation_val.odgt
+
+cd $output_dir
+for file in "CrowdHuman_train01" "CrowdHuman_train02" "CrowdHuman_train03" "CrowdHuman_val.zip"
+do
+    unzip $file.zip -d .
+done
