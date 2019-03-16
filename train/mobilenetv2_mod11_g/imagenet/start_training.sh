aaa748e7c9397eced06a010507fc8a7caeb86bd6
abdelrahman-gaber <eng.abdelrahman.gaber@gmail.com>
2019-03-16 21:25:05 +0000

    organize trained models

---
+++ train/mobilenetv2_mod11_g/imagenet/start_training.sh
@@ -0,0 +1,14 @@
+export PATH=/home/ubuntu/caffe-SSD:$PATH
+export PYTHONPATH=/home/ubuntu/caffe-SSD/python:/home/ubuntu/caffe-SSD:$PYTHONPATH
+
+CAFFE_ROOT=$(realpath "../../../../")
+
+mkdir -p logs snapshots
+
+#${CAFFE_ROOT}/build/tools/caffe train \
+#cd ../../../..
+
+#./build/tools/caffe train \
+${CAFFE_ROOT}/build/tools/caffe train \
+--solver=solver.prototxt \
+--gpu=0 2>&1 | tee logs/MobileNetV2-mod11g-ImageNetILSVRC12-CaffeSSD.log
