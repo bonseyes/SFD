809177aa1d9ed6231dd88ef830dcc4ac256f06de
abdelrahman-gaber <eng.abdelrahman.gaber@gmail.com>
2019-04-06 14:49:10 +0000

    add trained ImageNet models for n, i, and e

---
+++ train/mobilenetv2_mod11_i/imagenet/generate_sfd.sh
@@ -0,0 +1,10 @@
+
+stage=train
+python gen_model_fp16.py --classifier --stage $stage --lmdb /home/ubuntu/data/WIDER_FACE/lmdb/WIDER_FACE_train_lmdb_NVCaffe --label-map data/WIDER_FACE/labelmap_wider.prototxt --class-num 1000 > $stage.prototxt
+
+stage=test
+python gen_model_fp16.py --classifier --stage $stage --lmdb /home/ubuntu/data/WIDER_FACE/lmdb/WIDER_FACE_val_lmdb_NVCaffe --label-map data/WIDER_FACE/labelmap_wider.prototxt --class-num 1000 > $stage.prototxt
+
+stage=deploy
+python gen_model_fp16.py --classifier --stage $stage --lmdb /home/ubuntu/data/WIDER_FACE/lmdb/WIDER_FACE_val_lmdb_NVCaffe --label-map data/WIDER_FACE/labelmap_wider.prototxt --class-num 1000 > $stage.prototxt
+
